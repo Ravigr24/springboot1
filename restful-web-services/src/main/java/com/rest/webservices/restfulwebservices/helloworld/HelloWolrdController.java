69ea4636a44fcfb905f8ff91be9253cbe1900f26
Ravigr24 <88142537+Ravigr24@users.noreply.github.com>
2023-02-13 22:41:26 +0530

    Add files via upload

---
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWolrdController.java
@@ -0,0 +1,50 @@
+package com.rest.webservices.restfulwebservices.helloworld;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWolrdController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWolrdController(MessageSource messageSource) {
+		this.messageSource=messageSource;
+	}
+	
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	// Path Parameters
+	// /hello-world/path-variabel/Ranga
+	
+	@GetMapping(path="/hello-world/path-variabel/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		//return new HelloWorldBean("Hello World "+name);
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-Internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale=LocaleContextHolder.getLocale();
+	  	return messageSource.getMessage("good.mornig.message", null, "Default Message", locale);
+	}
+	
+	
+	
+
+
+}
